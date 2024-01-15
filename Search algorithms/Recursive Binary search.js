752ff52ae3bd1be228f36b9cb3c8a0fc30e79a8a
Irfan PK <pkirfanx@gmail.com>
2024-01-15 15:45:17 +0530

    git test

---
+++ Search algorithms/Recursive Binary search.js
@@ -0,0 +1,20 @@
+// recu
+
+
+// function recursivebinarysearch(arr, target, left, right) {
+//     if (left > right) {
+//         return -1
+//     }
+//     let mid = Math.floor((left + right) / 2)
+//     if (arr[mid] === target) {
+//         return mid
+//     } else if (arr[mid] < target) {
+//         return recursivebinarysearch(arr, target, mid + 1, right)
+//     } else {
+//         return recursivebinarysearch(arr, target, left, mid - 1)
+//     }
+
+// }
+
+// console.log(recursivebinarysearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10, 0, 9))
+let mid = Math.floor((left + right) / 2)
